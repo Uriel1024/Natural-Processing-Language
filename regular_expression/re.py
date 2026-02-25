1f9ca3543c3d6e2735f441a4f984edd5724ee10f
Uriel <evelazqueza1900@alumno.ipn.mx>
2026-02-24 18:38:08 -0600

    V1.2
    A version who accept february

--- regular_expression/re.py
+++ regular_expression/re.py
@@ -4,12 +4,15 @@ import re
 #caracter, inicio, enmedio y final  
 
 
-prueba = "01-02-2020"
+#prueba = "11-2-2023"
+prueba = input("Ingresa una fecha: ")
 #El primer bloque sirve para mostrar los dias del 01 al 09 0[1-9], el or es para los dias del 10 al 29 [12]01-9]  y el tercero es para el 30
 #El segundo valida si es un mes par hasta el 12
 #El tercer bloque es solo valida que tenga 4 digitos y que se encuentre al final
 #Despues se hace practicamente lo mismo con el bloque pero para meses que tienen 31 dias que son los impares
-patron = r"^((0[1-9]|[12][0-9]|3[0])-(2|4|6|8|10|12)-\d{4}$)|((0[1-9]|[12][0-9]|3[1])-(1|3|5|7|9|11)-\d{4}$)"
+
+patron = r"^((0[1-9]|[12][0-9]|3[0])-(4|6|8|10|12|04|08)-\d{4}$)|((0[1-9]|[12][0-9]|3[1])-(1|3|5|7|9|11|01|03|05|07|09)-\d{4}$)|((0[1-9]|1\d|2[0-8])-(2|02)-\d{4}$)"
+
 
 if re.search(patron,prueba):
     print(f"La fecha {prueba} es valida")
